241603244057bbc6f245ccfed64bc69a5e115ff4
alexander930828 <77578710+alexander930828@users.noreply.github.com>
2021-10-23 23:23:23 +0000

    today:

---
+++ DFS8.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+
+s = []
+
+def DFS():
+    if len(s) == m: 
+        print(' '.join(map(str, s)))
+        return
+
+    for i in range(1, n+1):
+        s.append(i)
+        DFS()
+        s.pop()
+
+DFS()
